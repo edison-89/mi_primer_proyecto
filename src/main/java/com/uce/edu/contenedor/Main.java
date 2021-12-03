30ac1123d847bdede5e476dc776b21b695af02dd
edison <edison@10.1.144.70>
2021-12-02 19:46:53 -0500

    Mi segundo taller

---
+++ src/main/java/com/uce/edu/contenedor/Main.java
@@ -0,0 +1,12 @@
+package com.uce.edu.contenedor;
+
+public class Main {
+	
+	private static final int IVA=12;//base de datos, o lo trae de una variable de entorno
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/com/uce/edu/contenedor/MainMaven.java
+++ src/main/java/com/uce/edu/contenedor/MainMaven.java
@@ -1,10 +1,10 @@
 package com.uce.edu.contenedor;
 
-public class Main {
+public class MainMaven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Mi primer proyecto maven");
 	}
 
 }
